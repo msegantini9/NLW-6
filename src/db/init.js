576e0ea7a6575e798fe92c773eb5fa1fca0a080c
Matheus Segantini de Almeida <m.segantini9@gmail.com>
2021-06-24 11:14:15 -0300

    Aula 4 - Persistindo os dados (criado server com node, criado rotas com express, criado banco de dados, passado os htmls para ejs, ...)

---
+++ src/db/init.js
@@ -0,0 +1,30 @@
+const Database = require("./config")
+
+const initDb = {
+    async init(){
+        const db = await Database()
+
+        await db.exec(`CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`);
+
+        await db.exec(`CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT,
+            read INT 
+        )`);
+
+        await db.exec(`CREATE TABLE answers (
+            questionId INT,
+            answerId INT,
+            text TEXT,
+            read INT  
+        )`);
+
+        await db.close()
+    }
+}
+
+initDb.init();
+
